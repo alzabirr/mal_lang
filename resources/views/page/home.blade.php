e161bfd328291599874c0ec68679c257cb6050e2
alzabirr <alzabirhussain@gmail.com>
2024-09-29 00:22:22 +0600

    hero area done

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -10,32 +10,24 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 
-
-
-
 </head>
 
 
 <style>
-
 .mal-font{
      font-family: "Press Start 2P"
 }
 
-
-
 </style>
-
-
 
 <body class="bg-slate-900">
 
 
-        <div class="relative isolate px-6 pt-14 lg:px-8">
+        <div class="relative isolate px-6 pt-20 lg:px-8">
             </div>
             <div class="text-center fon">
-              <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl font-Press mal-font " >Mal Lang 2.o</h1>
-              <p class="mt-6 text-lg leading-8 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque rem eius iusto inventore! Architecto laborum nulla optio maiores nobis qui..</p>
+              <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl font-Press mal-font  " >Mal Lang 2.o</h1>
+              <p class="mt-6 text-lg leading-8 text-white ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque rem eius iusto inventore! Architecto laborum nulla optio maiores nobis qui..</p>
 
             </div>
           </div>
@@ -43,7 +35,7 @@
 
 
           <div class="mt-20 flex items-center justify-center ">
-            <a href="#" class="rounded-md bg-indigo-600 px-4 py-4 text-2xl font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mal-font">Get started</a>
+            <a href="#" class="rounded-md bg-indigo-600 px-4 py-4 text-xl font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mal-font">Get started</a>
 
           </div>
 
